bc3178bdd40c8dc789fec5dae51d114d289d6c3f
Uri Cohen <uri1803@gmail.com>
2013-10-23 00:03:49 +0300

    restyled summary, new brand logo

--- _plugins/summary.rb
+++ _plugins/summary.rb
@@ -22,19 +22,23 @@ module Jekyll
       end
 
       def add_summary(context, content)
-      	output = "<div class='panel panel-primary'><div class='panel-heading'><h3 class='panel-title'>"        
+      	#output = "<div class='panel panel-primary'><div class='panel-heading'>"
+        #output << "<h3 class='panel-title'>"        
         #if @section
         #  output << "<strong>Section Summary: </strong>"
         #else
         #  output << "<strong>Summary: </strong></h3></p>"
         #end
+        output = "<h4 style='font-weight:300'>"
         output << Kramdown::Document.new(content).to_html
-        output << "</p>"
-        if @section
-          output << "</div></div>"
-        else
-          output << "</div><div class='panel-body'><ul class='nav nav-pills' id='summarypanel'></ul></div></div>"
-        end
+        output << "</h4><hr/>"
+        #if @section
+        #  output << "</div></div>"
+        #else
+          #output << "</div><div class='panel-body'><ul class='nav nav-pills' id='summarypanel'></ul></div></div>"
+        #end
+        output << "<ul class='nav nav-pills' id='summarypanel'></ul>"
+        output << "<hr/>"
       end
     end
   end
