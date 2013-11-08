847f80a6721a53bc7d1a0d52a7a31c93bdbf7be6
Uri Cohen <uri1803@gmail.com>
2013-11-08 18:13:02 +0200

    Scaladoc plugin

---
+++ _plugins/scaladoc.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  class Scaladoc < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @class_name = text.strip
+    end
+
+    def render(context)
+      create_link(context, super)
+    end
+
+    def create_link(context, content)
+      latest_xap_release = context.registers[:site].config["latest_xap_release"] || "9.7"
+
+      base_scaladoc_url = context.registers[:site].config["base_scaladoc_url"] || 
+                         "http://www.gigaspaces.com/docs/scaladocs%{version}/#%{fqcn}"
+
+      base_scaladoc_url % {:version => latest_xap_release, :fqcn => @class_name}
+    end
+  end
+end
+
+Liquid::Template.register_tag('scaladoc', Jekyll::Scaladoc)
