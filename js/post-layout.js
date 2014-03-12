16b64138e661a1c36aaf19defd424e3704c39384
Uri Cohen <uri1803@gmail.com>
2014-03-12 21:25:28 +0200

    Popup once per session

--- js/post-layout.js
+++ js/post-layout.js
@@ -98,16 +98,20 @@ $(function () {
     location.href=repo + "/edit/master" + markdownFile + "#";
   });
 
-  $("#edit-on-github").popover({
-        placement: 'left',
-        html: 'true',
-        title : '<span class="text-info"><strong>Help Us Improve!</strong></span>'+
-                '<button type="button" id="close" class="close" onclick="$(&quot;#edit-on-github&quot;).popover(&quot;hide&quot;);">&times;</button>',
-        content : 'Found a mistake in this page? Click here to edit it in Github and propose your change!'
-   });
+  var githubPopupPresented = sessionStorage.getItem('githubPopupPresented');
+  if (githubPopupPresented == null) {
+    $("#edit-on-github").popover({
+          placement: 'left',
+          html: 'true',
+          title : '<span class="text-info"><strong>Help Us Improve!</strong></span>'+
+                  '<button type="button" id="close" class="close" onclick="$(&quot;#edit-on-github&quot;).popover(&quot;hide&quot;);">&times;</button>',
+          content : 'Found a mistake in this page? Click here to edit it in Github and propose your change!'
+     });
 
-  setTimeout(function() {  
-    $('#edit-on-github').popover('show');
-  }, 500);
+    setTimeout(function() {  
+      $('#edit-on-github').popover('show');
+      sessionStorage.setItem('githubPopupPresented', 'true');
+    }, 500);
+  }
   
 })
